5bcdc0b146ddf34617479a64cb93352261fad579
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2022-09-01 19:27:44 +0900

    add text fields

--- browser-clipboard/src/options.js
+++ browser-clipboard/src/options.js
@@ -1,12 +1,35 @@
+const MAX = 5;
 const setOption = () => {
-	const text = document.getElementById('text').value;
+	const notes = document.getElementsByTagName('input');
+	const contents = [];
+	for (let i in notes) {
+		if (notes[i].value) {
+			contents.push(notes[i].value);
+		}
+	}
 	chrome.storage.sync
 		.set({
-			text: { text },
+			text: contents,
 		})
 		.then();
 };
-(() => {
+const getOption = async () => {
+	const notes = await chrome.storage.sync.get();
+	const contents = notes['text'];
+	let html = '';
+	for (let i in contents) {
+		if (!contents[i]) continue;
+		html += `<label><input type="text" value="${contents[i]}"></label><br>`;
+	}
+	const diff = MAX - contents.length;
+	for (let j = 0; j < diff; j++) {
+		html += `<label><input type="text"></label><br>`;
+	}
+	document.getElementById('form').insertAdjacentHTML('afterbegin', html);
+};
+
+(async () => {
+	await getOption();
 	document.getElementById('submit').addEventListener('click', e => {
 		e.preventDefault();
 		setOption();
--- browser-clipboard/src/popup.js
+++ browser-clipboard/src/popup.js
@@ -1,13 +1,19 @@
-const copy = () => {
-	const text = document.getElementById('text').textContent;
-	navigator.clipboard.writeText(text).then();
-};
-
 (async () => {
 	const data = await chrome.storage.sync.get();
-	const html = `<a id="text">${data.text.text}</a><button id="copy">copy</button>`;
+	const contents = data['text'];
+	let html = '';
+	for (let i in contents) {
+		if (!contents[i]) continue;
+		html += `<a class="${i}">${contents[i]}</a><button class="${i}">copy</button><br>`;
+	}
 	document
 		.getElementsByClassName('text')[0]
 		.insertAdjacentHTML('afterbegin', html);
-	document.getElementById('copy').addEventListener('click', copy);
+	document.addEventListener('click', e => {
+		if (!e) return;
+		const text = document.getElementsByClassName(e.target.className)[0]
+			.textContent;
+		console.log(text);
+		navigator.clipboard.writeText(text).then();
+	});
 })();

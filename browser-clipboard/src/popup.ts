dfba36d70afcf26e5334ebc25d50ee50928e67b2
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2022-10-12 18:18:52 +0900

    add eslint

--- browser-clipboard/src/popup.ts
+++ browser-clipboard/src/popup.ts
@@ -1,42 +1,40 @@
 const setOption = () => {
-	const notes = document.getElementsByTagName('input');
-	const contents = [];
-	for (let i in notes) {
-		if (notes[i].value) contents.push(notes[i].value);
-	}
-	chrome.storage.sync
-		.set({
-			text: contents,
-		})
-		.then();
-};
+  const notes = document.getElementsByTagName('input')
+  const contents = []
+  for (const i in notes) {
+    if (notes[i].value) contents.push(notes[i].value)
+  }
+  chrome.storage.sync
+    .set({
+      text: contents
+    })
+    .then()
+}
 const createField = (value: string, i: number) => {
-	return `<label><input type="text" value="${value}" class="${i}"></label><button class="${i}">copy</button><br>`;
-};
+  return `<label><input type="text" value="${value}" class="${i}"></label><button class="${i}">copy</button><br>`
+}
 const getOption = async () => {
-	const notes = await chrome.storage.sync.get();
-	const contents = notes['text'] || [];
-	let html = '';
-	for (let i in contents) {
-		if (!contents[i]) continue;
-		html += createField(contents[i], parseInt(i));
-	}
-	html += createField('', contents.length);
-	document.getElementById('form')?.insertAdjacentHTML('afterbegin', html);
+  const notes = await chrome.storage.sync.get()
+  const contents = notes.text || []
+  let html = ''
+  for (const i in contents) {
+    if (!contents[i]) continue
+    html += createField(contents[i], parseInt(i))
+  }
+  html += createField('', contents.length)
+  document.getElementById('form')?.insertAdjacentHTML('afterbegin', html)
 };
 
 (async () => {
-	await getOption();
-	document.addEventListener('click', (e: MouseEvent) => {
-		const target = e.target as HTMLInputElement;
-		if (target.tagName !== 'BUTTON') return;
-		setOption();
-		if (target.id !== 'submit') return;
-		const className = target.className;
-		const elements = document.getElementsByClassName(
-			className
-		) as HTMLCollectionOf<HTMLInputElement>;
-		if (!elements) return;
-		navigator.clipboard.writeText(elements[0].value).then();
-	});
-})();
+  await getOption()
+  document.addEventListener('click', (e: MouseEvent) => {
+    const target = e.target as HTMLInputElement
+    if (target.tagName !== 'BUTTON') return
+    setOption()
+    if (target.id !== 'submit') return
+    const element = document.getElementsByClassName(
+      target.className
+    )[0] as HTMLInputElement
+    navigator.clipboard.writeText(element.value).then()
+  })
+})()
